daa213d5d3783a379ffcae659a790dc00061e227
Yura <timtsa85@gmail.com>
2022-04-05 19:44:28 +0300

    End of section 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Extansions;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -24,7 +25,7 @@ namespace API
 {
     public class Startup
     {
-       
+
         private readonly IConfiguration _config;
 
         public Startup(IConfiguration config)
@@ -32,7 +33,7 @@ namespace API
             _config = config;
         }
 
-     
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -40,19 +41,19 @@ namespace API
             services.AddAplicationServices(_config);
             services.AddControllers();
             services.AddCors();
-            services.AddSwaggerGen(c => 
+            services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
-           services.AddIdentityServices(_config);
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExeptionMiddleware>();
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
@@ -60,7 +61,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthentication();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -28,7 +28,7 @@ export class NavComponent implements OnInit {
       
     }, error => {
       console.log(error);
-      this.toastr.error(error.error);
+      
     })
   }
 
